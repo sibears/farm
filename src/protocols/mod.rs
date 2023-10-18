e205353c936295cdbb7abbc5a6f3e2614dba8f48
x5113nc3x <vadimpirogovskiy2003@gmail.com>
2023-10-18 22:09:09 +0700

    add tcp proto

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -5,8 +5,10 @@ use phf::phf_map;
 use crate::{models::flag::Flag, settings::ProtocolConfig};
 
 use self::forcad_http::ForcAdHttp;
+use crate::protocols::ructf_tcp::RuCtfTcp;
 
 pub mod forcad_http;
+pub mod ructf_tcp;
 
 pub trait ProtocolHandler: Send + Sync {
     fn send_flags(&self, queue_flags: Vec<Flag>, config: &ProtocolConfig) -> Vec<Flag>;
@@ -14,4 +16,5 @@ pub trait ProtocolHandler: Send + Sync {
 
 pub static mut PROTOCOLS: phf::Map<&str, &dyn ProtocolHandler> = phf_map!{
     "forcad_http" => &ForcAdHttp
+    "ructf_tcp" => &RuCtfTcp
 };
