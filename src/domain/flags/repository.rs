8058e7d6aacf462ff2e39706927dab7a34882050
x5113nc3x <vadimpirogovskiy2003@gmail.com>
2024-09-28 22:43:38 +0700

    [WIP]

--- src/domain/flags/repository.rs
+++ src/domain/flags/repository.rs
@@ -4,15 +4,14 @@ use crate::domain::flags::entities::{Flag, NewFlag};
 pub trait FlagRepo {
     type FlagRepoError;
 
+    fn get(&self, id: i32) -> Result<Flag, Self::FlagRepoError>;
     fn get_all(&self) -> Result<Vec<Flag>, Self::FlagRepoError>;
-    fn get_by_id(&self, id: i32) -> Result<Flag, Self::FlagRepoError>;
     fn save(&self, flag: &NewFlag) -> Result<usize, Self::FlagRepoError>;
     fn save_all(&self, flag: &[NewFlag]) -> Result<usize, Self::FlagRepoError>;
-    fn delete_by_id(&self, id: i32) -> Result<usize, Self::FlagRepoError>;
+    fn delete(&self, id: i32) -> Result<usize, Self::FlagRepoError>;
+    fn delete_all(&self, flags: &[Flag]) -> Result<usize, Self::FlagRepoError>;
     fn update(&self, flag: &Flag) -> Result<usize, Self::FlagRepoError>;
-    fn skip_by_time(&self, skip_time: NaiveDateTime) -> Result<usize, Self::FlagRepoError>;
-    fn get_limit(&self, limit: i64) -> Result<Vec<Flag>, Self::FlagRepoError>;
     fn update_all(&self, flags: &[Flag]) -> Result<usize, Self::FlagRepoError>;
-    // fn skip_duplicate(&self, flags: Vec<NewFlag>) -> Result<Vec<NewFlag>, Self::ReposError>;
+    fn get_limit(&self, limit: i64) -> Result<Vec<Flag>, Self::FlagRepoError>;
     fn get_last_id(&self) -> Result<i32, Self::FlagRepoError>;
 }
