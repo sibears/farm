3a2c43d06cbcfd28dc67d792c31f5884bc25ce3f
x5113nc3x <vadimpirogovskiy2003@gmail.com>
2025-06-07 14:39:57 +0700

    changed for mypy

---
+++ flag_senders/protocols/__init__.py
@@ -0,0 +1,5 @@
+from .ructf_http import RuCtfHttpFlagSender
+from .ructf_tcp import RuCtfTcpFlagSender
+from .sibir_http import SibirCtfHttpFlagSender
+
+__all__ = ["RuCtfHttpFlagSender", "SibirCtfHttpFlagSender", "RuCtfTcpFlagSender"]
